8021e43de292347aa11db7f7f00b8efbc0c89708
Elbekgithub <allayorovelbek@gmail.com>
2020-07-28 00:55:18 +0500

    fav added

--- ads/views.py
+++ ads/views.py
@@ -9,12 +9,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from ads.models import Ad, Comment
+from ads.models import Ad, Comment, Fav
 from ads.forms import CreateForm,CommentForm
 
 class AdListView(ListView):
 	model = Ad
 	template_name = "ads/list.html"
+	def get(self, request) :
+		ad_list = Ad.objects.all()
+		favorites = list()
+		if request.user.is_authenticated:
+			# rows = [{'id': 2}]  (A list of rows)
+			rows = request.user.favorite_ads.values('id')
+			favorites = [ row['id'] for row in rows ]
+		ctx = {'ad_list' : ad_list, 'favorites': favorites}
+		retval = render(request, self.template_name, ctx)
+		return retval
 
 class AdDetailView(DetailView):
 	model = Ad
@@ -93,3 +103,33 @@ class CommentDeleteView(DeleteView):
 	def get_success_url(self):
 		ad = self.object.ad
 		return reverse('ads:ad_detail', args=[ad.id])
+
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+	def post(self, request, pk) :
+		print("Add PK",pk)
+		t = get_object_or_404(Ad, id=pk)
+		fav = Fav(user=request.user, ad=t)
+		try:
+			fav.save()  # In case of duplicate key
+		except IntegrityError as e:
+			pass
+		return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+	def post(self, request, pk) :
+		print("Delete PK",pk)
+		t = get_object_or_404(Ad, id=pk)
+		try:
+			fav = Fav.objects.get(user=request.user, ad=t).delete()
+		except Fav.DoesNotExist as e:
+			pass
+
+		return HttpResponse()
